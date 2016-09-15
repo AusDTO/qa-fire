d10bab52d5e9c2ea05f531e9f18208278e3d8765
tessereth <teresa.bradbury@digital.gov.au>
2016-09-16 09:08:00 +1000

    Enable views, sessions etc

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -22,10 +22,6 @@ module QaFire
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
     config.active_job.queue_adapter = :sidekiq
   end
 end

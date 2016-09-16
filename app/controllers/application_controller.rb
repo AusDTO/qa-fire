e8779d96ea4a396edde4ea854e0ba171408459c4
tessereth <teresa.bradbury@digital.gov.au>
2016-09-16 15:45:43 +1000

    Require the user to be signed in for relevant routes (closes #13) (#24)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   def new_session_path(_scope)
     new_user_session_path
   end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,14 +5,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     sign_in_and_redirect(@user, :event => :authentication)
     set_flash_message(:notice, :success, :kind => 'Github') if is_navigational_format?
   rescue ::Exceptions::NoValidEmailError
-    redirect_to root_path
+    redirect_to after_omniauth_failure_path_for(resource_name)
     set_flash_message(:alert, :failure, :kind => 'Github', reason: 'your account does not have a valid email address') if is_navigational_format?
-  end
-
-  protected
-
-  def after_omniauth_failure_path_for(_scope)
-    root_path
   end
 
 end

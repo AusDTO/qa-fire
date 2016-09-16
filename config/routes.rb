c2c0e81041a23bf18d2d79c09cfe0f4037e7dd95
tessereth <teresa.bradbury@digital.gov.au>
2016-09-16 15:44:20 +1000

    Require the user to be signed in for relevant routes (closes #13)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   devise_scope :user do
@@ -5,11 +6,13 @@ Rails.application.routes.draw do
     delete 'users/sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
   resource :github_webhooks, only: :create, defaults: { formats: :json }
-  require 'sidekiq/web'
-  mount Sidekiq::Web => '/queue'
 
-  root to: 'projects#index'
-  resources :projects, path: '/' do
-    resources :deploys
+  authenticate :user do
+    mount Sidekiq::Web => '/queue'
+
+    root to: 'projects#index'
+    resources :projects, path: '/' do
+      resources :deploys
+    end
   end
 end

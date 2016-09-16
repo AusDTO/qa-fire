bb586a9b014e30c2dad3d754bf54814a6cb2c731
ltankey <luke.tankey@gmail.com>
2016-09-16 14:41:47 +1000

    feat: add sensible seeds to match PR fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.find_or_create_by(repository: 'maxious/helloworld-html')
+Project.find_or_create_by(repository: 'maxious/helloworld-php')

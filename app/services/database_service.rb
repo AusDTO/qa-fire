32e61c05368e429ce1a0f0d6cd50aec87b60b499
Brendan Gordon <brendan.gordon@digital.gov.au>
2016-09-21 13:10:23 +1000

    Update manifest format - seed params are now per service

--- app/services/database_service.rb
+++ app/services/database_service.rb
@@ -5,38 +5,51 @@ class DatabaseService
   end
 
   def perform!
-    unless @app_manifest["qafire"] && @app_manifest["qafire"]["services"] && @app_manifest["qafire"]["services"][0]
-      puts "No services"
+    unless @app_manifest['qafire'] && @app_manifest['qafire']['services'] && !@app_manifest['qafire']['services'].empty?
+      puts 'No services'
       return
     end
 
     CloudFoundry.login
-    new_db_service = CloudFoundry.find_service(db_service_name)["resources"].empty?
+    new_db_service = CloudFoundry.find_service(db_service_name)['resources'].empty?
+
+    #TODO handle multiple services
 
     #TODO could refactor create_service so it works better with this service
     CloudFoundry.create_service(db_service_name,
-                                @app_manifest["qafire"]["services"][0]["type"],
-                                @app_manifest["qafire"]["services"][0]["plan"],
+                                @app_manifest['qafire']['services'][0]['type'],
+                                @app_manifest['qafire']['services'][0]['plan'],
                                 @deploy.full_name)
 
     unless new_db_service
-      puts 'Skipping populating database - service previously existed'
+      puts 'Skipping populating database - service already existed'
       return
     end
 
-    unless s3_bucket && s3_key
-      puts 'Skipping populating database - S3 bucket/key missing'
+    unless @app_manifest['qafire']['services']['seed']
+      puts 'no seed information in manifest'
       return
     end
 
-    s3 = Aws::S3::Client.new(:credentials => aws_creds)
-    Tempfile.open(['downloaded', '.sql']) do |file|
-      puts "Downloading '#{s3_key}' from S3..."
-      s3.get_object(response_target: file.path, bucket: s3_bucket, key: s3_key)
-      restore file.path
-    end
+    if @app_manifest['qafire']['services']['seed']['s3']
+      bucket = @app_manifest['qafire']['services']['seed']['s3']['bucket']
+      key = @app_manifest['qafire']['services']['seed']['s3']['key']
 
-    puts 'Restore complete'
+      unless bucket && key
+        puts 'Skipping seeding database - S3 bucket/key missing'
+        return
+      end
+
+      s3_client = Aws::S3::Client.new(:credentials => aws_creds)
+      Tempfile.open(['downloaded', '.sql']) do |file|
+        puts "Downloading '#{key}' from S3..."
+        s3_client.get_object(response_target: file.path, bucket: bucket, key: key)
+        restore file.path
+        puts 'Restore from s3 complete'
+      end
+    else
+      puts 'manifest did not contain seed location'
+    end
   end
 
   private
@@ -68,15 +81,7 @@ class DatabaseService
     "#{@deploy.full_name}-db"
   end
 
-  def s3_bucket
-    @app_manifest["qafire"]["s3_bucket"]
-  end
-
-  def s3_key
-    @app_manifest["qafire"]["s3_key"]
-  end
-
   def type
-    @app_manifest["qafire"]["services"][0]["type"]
+    @app_manifest['qafire']['services'][0]['type']
   end
 end

850e3f8eb8481f63bcb73f50a53f7c0fca39fc29
Brendan Gordon <brendan.gordon@digital.gov.au>
2016-09-23 19:00:36 +1000

    Add blue-green deployment (#94)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,8 +3,22 @@
 # Exit immediately if any commands return non-zero
 set -e
 
+# cause a pipeline (for example, curl -s http://sipb.mit.edu/ | grep foo) to produce a
+# failure return code if any command errors not just the last command of the pipeline.
+set -o pipefail
+
 # Output the commands we run
 set -x
 
-cf push qafire-sidekiq
-cf push qafire
+cf push qafire-sidekiq -f manifest-worker.yml
+
+# Update the blue app
+cf unmap-route qafire-blue apps.staging.digital.gov.au -n qafire
+cf push qafire-blue -f manifest-web.yml
+cf map-route qafire-blue apps.staging.digital.gov.au -n qafire
+
+# Update the green app
+cf unmap-route qafire-green apps.staging.digital.gov.au -n qafire
+cf push qafire-green -f manifest-web.yml
+cf map-route qafire-green apps.staging.digital.gov.au -n qafire
+
